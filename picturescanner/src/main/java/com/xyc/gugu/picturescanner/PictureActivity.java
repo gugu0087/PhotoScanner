54f2c8baf75b768c3d0d3faca05a80e0ebab47d7
许运昌 <xuyunchang@mzjmedia.net>
2018-05-22 11:39:52 +0800

    更新浏览数字标记

--- picturescanner/src/main/java/com/xyc/gugu/picturescanner/PictureActivity.java
+++ picturescanner/src/main/java/com/xyc/gugu/picturescanner/PictureActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -22,16 +24,16 @@ import java.util.List;
 
 public class PictureActivity extends FragmentActivity {
 
-    private ImageView ivPic;
     public static final String PIC_CONFIG = "picConfig";
     private ArrayList<String> picList;
     private ViewPager imgViewpager;
-    private FrameLayout rlLayout;
+    private RelativeLayout rlLayout;
+    private TextView tvImgCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >= 19) {
+        if (Build.VERSION.SDK_INT >= 19) {
             this.getWindow().addFlags(67108864);
         }
         setContentView(R.layout.activity_picture);
@@ -50,15 +52,34 @@ public class PictureActivity extends FragmentActivity {
         if (picList == null) {
             return;
         }
-        ImageAdapter adapter = new ImageAdapter(this,picList);
+        ImageAdapter adapter = new ImageAdapter(this, picList);
         imgViewpager.setAdapter(adapter);
         imgViewpager.setCurrentItem(pic_position);
+        tvImgCount.setText((pic_position+1) + "/" + picList.size());
+        imgViewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tvImgCount.setText((position+1) + "/" + picList.size());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     private void initView() {
-        ivPic = (ImageView) findViewById(R.id.ivPic);
-        imgViewpager = (ViewPager) findViewById(R.id.imgViewpager);
-        rlLayout = (FrameLayout) findViewById(R.id.rlLayout);
+
+        imgViewpager = findViewById(R.id.imgViewpager);
+        rlLayout = findViewById(R.id.rlLayout);
+        tvImgCount = findViewById(R.id.tvImgCount);
+
     }
 
     @Override
